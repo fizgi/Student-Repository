87f0abfd158c2fa138ff5290b8a8c8bb90b00e87
Fatih IZGI <izgifth@gmail.com>
2020-04-04 16:43:19 -0400

    added Remaining Required Courses, Remaining Elective Courses and GPA

--- Student_Repository_Fatih_IZGI.py
+++ Student_Repository_Fatih_IZGI.py
@@ -1,12 +1,12 @@
 """ A data repository of courses, students, and instructors.
 
     author: Fatih IZGI
-    date: 26-Mar-2020
+    date: 04-Apr-2020
     version: python 3.8.2
 """
 
 import os
-from typing import Dict, List, Iterator, Tuple, Union
+from typing import Dict, List, Iterator, Tuple, Union, Optional
 from collections import defaultdict
 from prettytable import PrettyTable
 
@@ -19,14 +19,29 @@ class Student:
         self.name: str = name
         self.major: str = major
         self.courses: Dict[str, str] = {}  # course {course_code: grade}
+        self.remaining_req: List[str] = []  # container for remaining required courses
+        self.remaining_elc: List[str] = []  # container for remaining elective courses
 
     def enroll_or_update(self, course: Dict[str, str]) -> None:
         """ enroll student to a course """
         self.courses.update(zip(course.keys(), course.values()))  # multiple courses can be added
 
-    def info(self) -> List[Union[str, List[str]]]:
+    def gpa(self) -> Optional[Union[int, float]]:
+        """ calculates and return the GPA of the student """
+        grades: Dict[str, float] = {"A": 4.00, "A-": 3.75, "B+": 3.25, "B": 3.00,
+                                    "B-": 2.75, "C+": 2.25, "C": 2.00, "C-": 0.00,
+                                    "D+": 0.00, "D": 0.00, "D-": 0.00, "F": 0.00}
+        try:
+            avg: float = sum([grades[grade] for grade in self.courses.values()]) / len(
+                self.courses.values())
+            return round(avg, 2)
+        except ZeroDivisionError:
+            return 0
+
+    def info(self) -> List[Union[str, str, str, List[str], List[str], List[str], int]]:
         """ returns the summary data about a single student """
-        return [self.cwid, self.name, sorted(self.courses.keys())]  # return student info as a list
+        return [self.cwid, self.name, self.major, sorted(self.courses.keys()),
+                sorted(self.remaining_req), sorted(self.remaining_elc), self.gpa()]
 
 
 class Instructor:
@@ -57,25 +72,36 @@ class Repository:
 
         self.students: Dict[str, Student] = dict()  # container {cwid: StudentObject}
         self.instructors: Dict[str, Instructor] = dict()  # container {cwid: InstructorObject}
+        self.departments: Dict[str, Dict[str, str]] = dict()
         self.dir_path: str = dir_path
 
         self.process_files()  # process the data in students, instructors and grades files
 
+    def department_info(self) -> Iterator[Tuple[str, List[str], List[str]]]:
+        """ a generator which yields the summary data about department """
+        for major, courses in self.departments.items():
+            req_list: List[str] = []  # = [name for name, r_e in courses.items() if r_e == "R"]
+            elc_list: List[str] = []  # = [name for name, r_e in courses.items() if r_e == "E"]
+
+            for name, r_e in courses.items():
+                (req_list if r_e == "R" else elc_list).append(name)
+
+            yield [major, sorted(req_list), sorted(elc_list)]
+
     def process_files(self) -> None:
         """ process the data in students, instructors and grades files """
-        for cwid, name, major in self.file_reader("students.txt", 3, sep=";", header=True):
+        for cwid, name, major in \
+                file_reader(os.path.join(self.dir_path, "students.txt"), 3, sep=";", header=True):
             student: Student = Student(cwid, name, major)
             self.students[cwid] = student  # add the new student to the container
 
-        for cwid, name, department in self.file_reader("instructors.txt", 3, sep="|", header=True):
+        for cwid, name, department in \
+                file_reader(os.path.join(self.dir_path, "instructors.txt"), 3, "|", True):
             instructor: Instructor = Instructor(cwid, name, department)
             self.instructors[cwid] = instructor
 
-        for cwid, name, department in self.file_reader("majors.txt", 3, sep="\t", header=True):
-            instructor: Instructor = Instructor(cwid, name, department)
-            self.instructors[cwid] = instructor
-
-        for std_cwid, course, letter_grade, inst_cwid in self.file_reader("grades.txt", 4, sep="|", header=True):
+        for std_cwid, course, letter_grade, inst_cwid in \
+                file_reader(os.path.join(self.dir_path, "grades.txt"), 4, sep="|", header=True):
             if std_cwid not in self.students.keys():
                 raise ValueError(f"A grade for an unknown student with cwid: {std_cwid}")
 
@@ -83,37 +109,34 @@ class Repository:
                 raise ValueError(f"A grade for an unknown instructor with cwid: {inst_cwid}")
 
             # find the student and instructor by cwid and update their course list
-            self.students[std_cwid].enroll_or_update({course: letter_grade})
+            if letter_grade in ["A", "A-", "B+", "B", "B-", "C+", "C"]:
+                self.students[std_cwid].enroll_or_update({course: letter_grade})
             self.instructors[inst_cwid].add_or_update(course)
+
+        for major, req_elc, course in \
+                file_reader(os.path.join(self.dir_path, "majors.txt"), 3, sep="\t", header=True):
+            if major in self.departments:
+                self.departments[major][course] = req_elc
+            else:
+                self.departments[major] = {course: req_elc}
+
+        for student in self.students.values():
+            for course, r_e in self.departments[student.major].items():
+                if course not in student.courses:
+                    (student.remaining_req if r_e == "R" else student.remaining_elc).append(course)
 
         self.pretty_print()
 
-    def file_reader(self, path: str, fields: int, sep: str = "\t", header: bool = False) \
-            -> Iterator[List[str]]:
-        """ a generator function to read field-separated text files
-            and yield a tuple with all of the values from a single line """
-        if not os.path.exists(os.path.join(self.dir_path, path)):
-            raise FileNotFoundError(f"The specified directory"
-                                    f"‘{os.path.join(self.dir_path, path)}’ is not found")
-
-        file = open(os.path.join(self.dir_path, path), "r")
-
-        with file:  # close file after opening
-            for line_number, line in enumerate(file, 1):
-                row_fields: List[str] = line.rstrip("\n").split(sep)
-                row_field_count: int = len(row_fields)
-
-                if row_field_count != fields:
-                    raise ValueError(f"‘{path}’ has {row_field_count} fields "
-                                     f"on line {line_number} but expected {fields}")
-
-                if not header or line_number != 1:
-                    yield row_fields
-
     def pretty_print(self) -> None:
         """ prettify the data """
+        department_table: PrettyTable = PrettyTable()
+        department_table.field_names = ["Major", "Required Courses", "Electives"]
+        for info in self.department_info():
+            department_table.add_row(info)  # add department info to the table
+
         student_table: PrettyTable = PrettyTable()
-        student_table.field_names = ["CWID", "Name", "Completed Courses"]
+        student_table.field_names = ["CWID", "Name", "Major", "Completed Courses",
+                                     "Remaining Required", "Remaining Electives", "GPA"]
         for student in self.students.values():
             student_table.add_row(student.info())  # add student info to the table
 
@@ -123,13 +146,38 @@ class Repository:
             for info in instructor.info():
                 instructor_table.add_row(info)  # add instructor info to the table
 
+        print(department_table)
         print("Student Summary\n", student_table, sep="")
         print("Instructor Summary\n", instructor_table, sep="")
 
 
+def file_reader(path: str, fields: int, sep: str = "\t", header: bool = False) \
+        -> Iterator[List[str]]:
+    """ a generator function to read field-separated text files
+        and yield a tuple with all of the values from a single line """
+    if not os.path.exists(path):
+        raise FileNotFoundError(f"The specified directory"
+                                f"‘{path}’ is not found")
+
+    file = open(path, "r")
+
+    with file:  # close file after opening
+        for line_number, line in enumerate(file, 1):
+            row_fields: List[str] = line.rstrip("\n").split(sep)
+            row_field_count: int = len(row_fields)
+
+            if row_field_count != fields:
+                raise ValueError(f"‘{path}’ has {row_field_count} fields "
+                                 f"on line {line_number} but expected {fields}")
+
+            if not header or line_number != 1:
+                yield row_fields
+
+
 def main():
     """ the main class to get the data for each organization """
-    stevens = Repository("stevens") # read files and generate prettytables
+    stevens = Repository("stevens")  # read files and generate prettytables
+    stevens.department_info()
     # njit = Repository("njit")
     # test = Repository("test")
 
